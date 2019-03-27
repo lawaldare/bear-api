b8cf1a3c7bb959f43fd3fe8b2a9b3ab0228ff3aa
Dare Lawal <lawaldare@gmail.com>
2019-03-27 14:57:55 +0100

    entry point set up

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = require('./routes/api');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//set up express app
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+//configure app to use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//initialize routes
+app.use('/api', routes);
+
+//error handling middleware
+app.use(function(err, req, res, next) {
+	res.status(422).send({ error: err.message });
+});
+
+//listening for requests
+app.listen(process.env.PORT || 8080, function() {
+	console.log('now listening for request...');
+});
