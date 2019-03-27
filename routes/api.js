a21fbad178186b2594a3e8868b8e86918776faf7
Dare Lawal <lawaldare@gmail.com>
2019-03-27 17:53:11 +0100

    codes refactored

--- routes/api.js
+++ routes/api.js
@@ -3,40 +3,55 @@ const router = express.Router();
 const Bear = require('../models/bears');
 
 //get a bear from the db by the ID
-router.get('/bears/:id', function(req, res, next) {
-	Bear.findById({ _id: req.params.id }).then(function(bear) {
-		res.send(bear);
-	});
+router.get('/bears/:id', (req, res, next) => {
+	Bear.findById({ _id: req.params.id }).then(
+		(bear) => {
+			res.send(bear);
+		},
+		(e) => {
+			res.status(400).send(e);
+		}
+	);
 });
 
 //get all the list of bear from the database
-router.get('/bears', function(req, res, next) {
-	Bear.find().then((bear) => {
-		res.send({ bear });
-	});
+router.get('/bears', (req, res, next) => {
+	Bear.find().then(
+		(bear) => {
+			res.send({ bear });
+		},
+		(e) => {
+			res.status(400).send(e);
+		}
+	);
 });
 
 //add a new ninja to the db
-router.post('/bears', function(req, res, next) {
+router.post('/bears', (req, res, next) => {
 	Bear.create(req.body)
-		.then(function(bear) {
+		.then((bear) => {
 			res.send(bear);
 		})
 		.catch(next);
 });
 
 //update a ninja in the db
-router.put('/bears/:id', function(req, res, next) {
-	Bear.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
-		Bear.findOne({ _id: req.params.id }).then(function(bear) {
-			res.send(bear);
-		});
+router.put('/bears/:id', (req, res, next) => {
+	Bear.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+		Bear.findOne({ _id: req.params.id }).then(
+			(bear) => {
+				res.send(bear);
+			},
+			(e) => {
+				res.status(400).send(e);
+			}
+		);
 	});
 });
 
 //delete a ninja from the db
-router.delete('/bears/:id', function(req, res, next) {
-	Bear.findByIdAndRemove({ _id: req.params.id }).then(function(bear) {
+router.delete('/bears/:id', (req, res, next) => {
+	Bear.findByIdAndRemove({ _id: req.params.id }).then((bear) => {
 		res.send(bear);
 	});
 });
